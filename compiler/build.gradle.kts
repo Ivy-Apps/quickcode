f701375851aa2c5e87fece1ddb470e2555702149
iliyangermanov <iliyan.germanov971@gmail.com>
2023-10-11 20:42:08 +0300

    Init the compiler project

---
+++ compiler/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "com.ivy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
